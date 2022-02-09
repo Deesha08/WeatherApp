d7134250e7d56f55ddf3c34ebac5335cd873319a
ddesai-sjsu <deesha.desai6@gmail.com>
2022-02-08 17:00:28 -0800

    Adding swagger

--- weather-server/app.js
+++ weather-server/app.js
@@ -7,15 +7,18 @@ const weatherRouter = require('./routes/weather');
 
 const cors=require('cors')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output_final.json')
+
 const app = express();
 const port = 5000
-
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 app.use(recordRequest)
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/', indexRouter);
 app.use('/weather', weatherRouter);
